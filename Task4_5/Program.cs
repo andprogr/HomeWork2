6f131812cd8b8d76d4d4b062d99f887806cae7eb
andprogr <andprogram2025@outlook.com>
2025-03-17 21:28:23 +0300

    Task_4

---
+++ Task4_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            do
+            {
+                Console.WriteLine("Введите число от 20 до 60");
+                d = Convert.ToInt32(Console.ReadLine());
+                if (d <= 60 && d >= 20) break;
+            }
+            while (true);
+            Console.WriteLine("Число в диапазоне");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task4_7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());// кол-во цифр в цисле
+            int k = 0;
+            while (n > 0)
+            {
+                k++;
+                n /=10;
+                
+            }
+            Console.WriteLine("кол-во цифр" + k);
+            Console.ReadKey();
+        }
+
+    }
+}
